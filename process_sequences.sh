4aa2c9db9ad9ca09d12af0e3390972f6a87e0e30
fsmalik <59219252+fsmalik@users.noreply.github.com>
2024-05-20 14:39:28 -0400

    Add files via upload
    Maestro exports fasta sequences in a 'new and improved' format as of Release 2022-2. This file will adjust the sequences back to the original format for compatibility with the rest of the scripts.

---
+++ process_sequences.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+input_file="$1"
+
+output_file="$2"
+
+rm "$output_file"
+
+first_line=true
+sequence=""
+
+while IFS= read -r line || [[ -n $line ]]
+do
+    if [[ $line == \>* ]]; then
+        if [ "$first_line" = false ]; then
+            echo "$sequence" >> "$output_file"
+            echo "" >> "$output_file"
+        fi
+
+        echo "$line" >> "$output_file"
+        sequence=""
+        first_line=false
+    else
+        sequence+="${line//-/}"
+    fi
+done < "$input_file"
+
+if [ -n "$sequence" ]; then
+    echo "$sequence" >> "$output_file"
+fi
+
+echo "" >> "$output_file"
